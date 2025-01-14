fe90f94b9c1a32e87a140774b4d69fc8a7b11b27
roshannawkar <roshannawkar899@gmail.com>
2025-01-14 14:09:49 +0530

    chanedf

--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function home() {
-  return <div>home</div>;
-}
+const home = () => {
+  return (
+    <>
+      <h1> mai hu f1 </h1>
+    </>
+  );
+};
+
+export default home;
