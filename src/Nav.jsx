c52bc44a297b329a9f3aa2eb573c2d5671568219
roshannawkar <roshannawkar899@gmail.com>
2025-01-14 22:28:22 +0530

    Added

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import ContactsIcon from "@mui/icons-material/Contacts";
 
 function Nav() {
   return (
     <div>
-      <header class="  text-gray-600 body-font">
-        <div class="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
-          <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <a class="mr-5 font-bold hover:text-gray-900">
-              <img className=" h-10" src="assets/Logo1.png" alt="" />
+      <header className=" mb-5 text-gray-600 body-font sticky top-0 z-50 bg-white shadow-[0_4px_6px_rgba(0,0,0,0.1),0_1px_3px_rgba(0,0,0,0.08)]">
+        <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center space-x-5">
+            <a className="font-bold hover:text-gray-900">
+              <img className="h-10" src="assets/Logo1.png" alt="" />
             </a>
-            <a class="mr-5 hover:text-yellow-500">
-              <span>| </span> &nbsp; Home
+            <a className="relative group text-base font-medium hover:text-yellow-500">
+              &nbsp; Home
+              <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-yellow-500 scale-x-0 group-hover:scale-x-100 transition-all duration-300"></span>
             </a>
-            <a class="mr-5 hover:text-yellow-500">More</a>
-            <a class="mr-5 hover:text-yellow-500">Listing</a>
-            <a class="mr-5 hover:text-yellow-500">Contact</a>
+            <a className="relative group text-base font-medium hover:text-yellow-500">
+              More
+              <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-yellow-500 scale-x-0 group-hover:scale-x-100 transition-all duration-300"></span>
+            </a>
+            <a className="relative group text-base font-medium hover:text-yellow-500">
+              Listing
+              <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-yellow-500 scale-x-0 group-hover:scale-x-100 transition-all duration-300"></span>
+            </a>
+            <a className="relative group text-base font-medium hover:text-yellow-500">
+              Contact &nbsp; <ContactsIcon />
+              <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-yellow-500 scale-x-0 group-hover:scale-x-100 transition-all duration-300"></span>
+            </a>
           </nav>
         </div>
       </header>
--- src/Sliding.jsx
+++ src/Sliding.jsx
@@ -31,7 +31,10 @@ const SlidingCarousel = () => {
   }, []);
 
   return (
-    <div id="default-carousel" className="relative w-full">
+    <div
+      id="default-carousel"
+      className=" shadow-lg shadow-gray-500/50 ... relative w-full"
+    >
       {/* Carousel wrapper */}
       <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
         {slides.map((slide, index) => (
@@ -67,55 +70,6 @@ const SlidingCarousel = () => {
       </div>
 
       {/* Slider controls */}
-      <button
-        type="button"
-        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={prevSlide}
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-
-      <button
-        type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={nextSlide}
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
     </div>
   );
 };
